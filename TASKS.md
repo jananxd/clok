91a7e2d763d94efec92380c512c263a85a9fd029
John Carlo Miel <johncarlomiel@gmail.com>
2026-08-22 18:29:39 +0800

    initial application

---
+++ TASKS.md
@@ -0,0 +1,7 @@
+# The purpose of this file is to have something we can use as note taking inside the editor and to know what specific things we need to do in a task. Make the checklist smaller task as possible so that we don't get bored because it's too hard.
+
+## August 22, 2026
+- [x] Create a box for the initial view, it should have the instructions on how to quit the program and how to create a new task.
+- [x] Establish the [TEA](https://ratatui.rs/concepts/application-patterns/the-elm-architecture/) architecture, make it as minimal as possible.
+- [x] Create a local list for the tasks.
+- [ ] Create a view to create task, it should only ask for name and description (optional) then when submitted we want to render it to the list and go back to the initial view.
--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,207 @@
-use crossterm::event::{self, KeyCode};
-use ratatui::Frame;
-use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::style::{Color, Modifier, Style, Stylize};
-use ratatui::text::{Line, Span};
-use ratatui::widgets::{List, ListDirection, ListState};
+use std::time::Duration;
+
+use ratatui::{
+    Frame,
+    crossterm::event::{self, Event, KeyCode},
+    layout::Alignment,
+    style::Style,
+    text::Line,
+    widgets::{Block, Borders, List, ListState, Paragraph, Wrap},
+};
+
+#[derive(Debug, Default)]
+struct Model {
+    running_state: RunningState,
+    tasks: Vec<Task>,
+    list_state: ListState,
+    app_view: AppView,
+}
+
+#[derive(Debug, Clone)]
+struct Task {
+    name: String,
+    description: Option<String>,
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+enum RunningState {
+    #[default]
+    Running,
+    Done,
+}
+
+#[derive(Debug, Default)]
+enum AppView {
+    #[default]
+    ListView,
+    CreateTask,
+}
+
+#[derive(PartialEq)]
+enum Message {
+    Increment,
+    Decrement,
+    Quit,
+    CreateTask,
+    CancelCreateTask,
+}
+
+fn update(model: &mut Model, msg: Message) -> Option<Message> {
+    match msg {
+        Message::Increment => {
+            model.list_state.select_next();
+        }
+        Message::Decrement => {
+            model.list_state.select_previous();
+        }
+        Message::Quit => {
+            // You can handle cleanup and exit here
+            model.running_state = RunningState::Done;
+        }
+        Message::CreateTask => {
+            model.app_view = AppView::CreateTask;
+        }
+        Message::CancelCreateTask => {
+            model.app_view = AppView::ListView;
+        }
+    };
+    None
+}
 
 fn main() -> color_eyre::Result<()> {
-    color_eyre::install()?;
+    tui::install_panic_hook();
+    let mut terminal = tui::init_terminal()?;
+    let mut model = Model::default();
+    model.app_view = AppView::ListView;
+    model.tasks.push(Task {
+        name: "Task 1".to_string(),
+        description: Some("description".to_string()),
+    });
 
-    let mut list_state = ListState::default().with_selected(Some(0));
-    ratatui::run(|terminal| {
-        loop {
-            terminal.draw(|frame| render(frame, &mut list_state))?;
-            if let Some(key) = event::read()?.as_key_press_event() {
-                match key.code {
-                    KeyCode::Char('j') | KeyCode::Down => list_state.select_next(),
-                    KeyCode::Char('k') | KeyCode::Up => list_state.select_previous(),
-                    KeyCode::Char('c') => println!("pressing C!!"),
-                    KeyCode::Char('q') | KeyCode::Esc => break Ok(()),
-                    _ => {}
-                }
-            }
+    model.tasks.push(Task {
+        name: "Task 2".to_string(),
+        description: Some("description".to_string()),
+    });
+
+    model.tasks.push(Task {
+        name: "Task 3".to_string(),
+        description: Some("description".to_string()),
+    });
+
+    model.list_state.select_first();
+
+    while model.running_state != RunningState::Done {
+        // Render the current view
+        terminal.draw(|f| view(&mut model, f))?;
+
+        // Handle events and map to a Message
+        let mut current_msg = handle_event(&model)?;
+
+        // Process updates as long as they return a non-None message
+        while current_msg.is_some() {
+            current_msg = update(&mut model, current_msg.unwrap());
         }
-    })
+    }
+
+    tui::restore_terminal()?;
+    Ok(())
 }
 
-/// Render the UI with various lists.
-fn render(frame: &mut Frame, list_state: &mut ListState) {
-    let constraints = [
-        Constraint::Length(1),
-        Constraint::Fill(1),
-        Constraint::Fill(1),
-    ];
-    let layout = Layout::vertical(constraints).spacing(1);
-    let [top, first, _] = frame.area().layout(&layout);
+fn view(model: &mut Model, frame: &mut Frame) {
+    match model.app_view {
+        AppView::CreateTask => {
+            let p = Paragraph::new("Hello!")
+                .block(Block::bordered().title("Paragraph"))
+                .style(Style::new().white().on_black())
+                .alignment(Alignment::Center)
+                .wrap(Wrap { trim: true });
 
-    let title = Line::from_iter([
-        Span::from("Clok").bold(),
-        Span::from(" (Press 'q' to quit and arrow keys to navigate)"),
-    ]);
+            frame.render_widget(p, frame.area());
+        }
+        AppView::ListView => {
+            let task_names: Vec<&str> = model.tasks.iter().map(|task| task.name.as_str()).collect();
 
-    frame.render_widget(title.centered(), top);
+            let b = Block::default()
+                .title(Line::from("Press \"q\" to exit").left_aligned())
+                .title(Line::from("Clok").centered())
+                .title(Line::from("Press \"c\" to create new task").right_aligned())
+                .borders(Borders::ALL);
 
-    render_list(frame, first, list_state);
+            let list = List::new(task_names)
+                .block(b)
+                .style(Style::new().white())
+                .highlight_style(Style::new().red().italic())
+                .highlight_symbol("> ")
+                .repeat_highlight_symbol(true);
+
+            frame.render_stateful_widget(list, frame.area(), &mut model.list_state);
+        }
+    }
 }
 
-/// Render a list.
-pub fn render_list(frame: &mut Frame, area: Rect, list_state: &mut ListState) {
-    let items = ["Item 1", "Item 2", "Item 3", "Item 4"];
-    let list = List::new(items)
-        .style(Color::White)
-        .highlight_style(Modifier::REVERSED)
-        .highlight_symbol("> ");
+fn handle_event(model: &Model) -> color_eyre::Result<Option<Message>> {
+    if event::poll(Duration::from_millis(250))?
+        && let Event::Key(key) = event::read()?
+        && key.kind == event::KeyEventKind::Press
+    {
+        return Ok(handle_key(key, model));
+    }
+    Ok(None)
+}
 
-    frame.render_stateful_widget(list, area, list_state);
+fn handle_key(key: event::KeyEvent, model: &Model) -> Option<Message> {
+    match key.code {
+        KeyCode::Char('q') => Some(Message::Quit),
+        _ => match model.app_view {
+            AppView::CreateTask => match key.code {
+                KeyCode::Esc => Some(Message::CancelCreateTask),
+                _ => None,
+            },
+            AppView::ListView => match key.code {
+                KeyCode::Char('j') => Some(Message::Increment),
+                KeyCode::Char('k') => Some(Message::Decrement),
+                KeyCode::Char('c') => Some(Message::CreateTask),
+                _ => None,
+            },
+        },
+    }
+}
+
+mod tui {
+    use ratatui::{
+        Terminal,
+        backend::{Backend, CrosstermBackend},
+        crossterm::{
+            ExecutableCommand,
+            terminal::{
+                EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode,
+            },
+        },
+    };
+    use std::{
+        io::{self, stdout},
+        panic,
+    };
+
+    pub fn init_terminal() -> color_eyre::Result<Terminal<impl Backend<Error = io::Error>>> {
+        enable_raw_mode()?;
+        stdout().execute(EnterAlternateScreen)?;
+        let terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
+        Ok(terminal)
+    }
+
+    pub fn restore_terminal() -> color_eyre::Result<()> {
+        stdout().execute(LeaveAlternateScreen)?;
+        disable_raw_mode()?;
+        Ok(())
+    }
+
+    pub fn install_panic_hook() {
+        let original_hook = panic::take_hook();
+        panic::set_hook(Box::new(move |panic_info| {
+            stdout().execute(LeaveAlternateScreen).unwrap();
+            disable_raw_mode().unwrap();
+            original_hook(panic_info);
+        }));
+    }
 }
